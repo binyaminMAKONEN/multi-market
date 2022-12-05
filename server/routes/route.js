5a21250f4a3a104af3429d44aa74a9a2301a9ec3
benjamin <bnymynmqwnn@gmail.com>
2022-12-05 21:40:41 +0200

    userController

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 const storesController = require('../controller/storeController');
 const productController = require('../controller/productController')
-
+const userController = require('../controller/usercontroller')
 
 routes.route('/api/stores')
      .post(storesController.createStore)
@@ -11,6 +11,13 @@ routes.route('/api/stores')
 routes.route('/api/products')
      .get(productController.getProduct)
      .post(productController.createProduct)
+
+
+routes.route('/api/users/register')
+     .post(userController.createUser)
+
+routes.route('/api/users/login')
+     .post(userController.login)
 
 
 
