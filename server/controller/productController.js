89c67fce1ed27139555bb92f056601ad247a8373
Meir Amara <meir757@gmail.com>
2022-12-06 23:05:00 +0200

    06/12/2022

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -1,11 +1,24 @@
 const product = require('../models/productModel')
 const {addProducts} =require("../methods/productsMethod")
 
-// get all store in localhost:8000
+// get all products in localhost:8000
 
-const getProduct = async (req,res)=>{
+const getProducts = async (req,res)=>{
     let data = await  product.find({})
     return res.json(data)
+}
+
+// get products by sore id
+
+const getProductsByStoreId=async(req,res)=>{
+  if(!req.body)return res.status(400).json('Post HTTP Data not Provided')
+  try {
+    const storeId=req.body
+    const data=await productsStore(storeId)//this function get id store and return all products that in store
+    res.status(200).json(data)
+ } catch (err) {
+    return res.status(400).json({message : `Error while creating product ${err}`})
+ }
 }
 
 // create store in localhost:8000
@@ -22,7 +35,9 @@ const createProduct = async(req,res)=>{
  }
     
 }
+
+
 module.exports = {
-    getProduct,
+    getProducts,
   createProduct
 }
