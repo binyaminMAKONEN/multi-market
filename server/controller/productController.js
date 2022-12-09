baa4ea3fdfe642c053b0bd0687f6db20d78d260f
Wubet zemele <wubet777@gmail.com>
2022-12-09 13:09:30 +0200

    I created a productRouter folder where there are the routes to the controller where there are functions of the crud one delete function that deletes a product and updates it both in the store and an update function that updates a product and in the store

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -1,10 +1,12 @@
-const product = require('../models/productModel')
-const {addProducts} =require("../methods/productsMethod")
+const Product = require('../models/productModel');
+const Store =require('../models/storeModel');
+const {addProducts} =require("../methods/productsMethod");
+const { default: mongoose } = require('mongoose');
 
 // get all store in localhost:8000
 
 const getProduct = async (req,res)=>{
-    let data = await  product.find({})
+    let data = await  Product.find({})
     return res.json(data)
 }
 
@@ -22,7 +24,46 @@ const createProduct = async(req,res)=>{
  }
     
 }
+const updateProduct = async (req,res)=>{
+  try {
+    const obj = {
+      name: req.body.name,
+      img: req.body.img,
+      quantity: req.body.quantity,
+      price: req.body.price,
+      productType: req.body.productType,
+      description: req.body.description,
+    };
+    console.log(obj);
+    const updateProducts = await Product.findByIdAndUpdate(
+      req.params.id,
+      {$set:obj}
+      
+    )
+    res.status(200).json(updateProducts)
+  } catch (error) {
+    throw error
+    
+  }
+}
+  const deleteProduct = async (req, res) => {
+    const productId=req.params.id
+    if (!mongoose.Types.ObjectId.isValid(productId)){
+      return res.status(404).send("not working")
+    }
+  
+   const product= await Product.findByIdAndDelete(productId)
+   const storeId = product.storeId.toString()
+   console.log(storeId);
+   
+    await Store.findByIdAndUpdate(storeId,{$pull:{products:{productId}}})
+    res.status(200).json('deleted product')
+ 
+  }
+
 module.exports = {
     getProduct,
-  createProduct
+  createProduct,
+  updateProduct,
+  deleteProduct
 }
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,16 +1,22 @@
 const routes = require('express').Router();
 const storesController = require('../controller/storeController');
-const productController = require('../controller/productController')
+// const productController = require('../controller/productController')
 const userController = require('../controller/usercontroller')
+const orderController = require('../controller/orderController')
 
 routes.route('/api/stores')
      .post(storesController.createStore)
      .get(storesController.getStores)
 
+routes.route('/api/orders')
+     // .post(storesController.createStore)
+     .get(orderController.getOrders)
+     .post(orderController.createOrders)
 
-routes.route('/api/products')
-     .get(productController.getProduct)
-     .post(productController.createProduct)
+
+// routes.route('/api/products')
+//      .get(productController.getProduct)
+//      .post(productController.createProduct)
 
 
 routes.route('/api/users/register')
