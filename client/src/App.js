c5d2f25c882ce42b055a879da0a315d8c04634ba
chen wassi <chenwassi1@gmail.com>
2022-12-11 15:20:54 +0200

    add client model

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,8 @@ import {Route,Routes} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <Routes>
+      <Routes>  
+       
        
       </Routes>
         {/* <Navbar/> */}
