f3c48f956d70d9a4020d80226937b1c13f8883c2
Meir Amara <meir757@gmail.com>
2022-12-11 22:07:06 +0200

    update user mudele

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,5 +1,6 @@
 
 const {getProduct,createProducts, updateProduct, deleteProduct} =require('../controller/productController') 
+const {tokenMiddleware} =require('../controller/authenticateToken')
 const router = require('express').Router()
 
 
