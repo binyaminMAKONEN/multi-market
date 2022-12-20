ce738a8d1983bb0f8a7281fab00f64fe96129ef2
Aviv Adga <avivadga64@gmail.com>
2022-12-20 15:56:06 +0200

    aviv

--- server/routes/route.js
+++ server/routes/route.js
@@ -6,13 +6,13 @@ const orderController = require('../controller/orderController')
 
 
 routes.route('/api/stores')
-.post(storesController.createStore)
-.get(storesController.getStores)
+.post(storesController.createStore)//premission to costumer and ownerstore
+.get(storesController.getStores)//everyone
 
 
 routes.route('/api/orders')
-     .get(orderController.getOrders)
-     .post(orderController.createOrders)
+     .get(orderController.getOrders)//premission to admin
+     .post(orderController.createOrders)// premission to any user that log in
 
 routes.route('/api/orders/:id')
      .get(orderController.getStoreOrCustomerOrderById)
@@ -23,9 +23,9 @@ routes.route('/api/orders/:id')
 //      .post(productController.createProduct)
 
 routes.route('/api/stores/:id')
-.delete(storesController.deleteStore)
-.put(storesController.updateStore)
-.get(storesController.getProductsStoreById)
+.delete(storesController.deleteStore)//premission to admin and owner store
+.put(storesController.updateStore)//premission to admin and owner store
+.get(storesController.getProductsStoreById)//premission to admin and owner store
 
 
 
@@ -33,6 +33,7 @@ routes.route('/api/stores/:id')
 
 routes.route('/api/users/register')
      .post(userController.createUser)
+     .get(userController.getUsers)//premission to admin
 
 routes.route('/api/users/login')
      .post(userController.login)
