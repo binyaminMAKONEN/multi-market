ac1e1083707d3cf20652f79e327f8784a40d5710
benjamin <bnymynmqwnn@gmail.com>
2022-12-20 03:00:32 +0200

    redux store

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Provider } from 'react-redux';
+import {store} from "./store/store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Provider store={store}>
+
     <BrowserRouter>
     <GoogleOAuthProvider clientId="350337376364-qmp2huls1vvbkqs7gou50geq807pdrr3.apps.googleusercontent.com"><App /></GoogleOAuthProvider>;
     
     </BrowserRouter>
+    </Provider>
 );
