eec7a299b798638a4b79eee4a06d4f3b0c4fb2cb
amir <nagat1700@gmail.com>
2022-12-21 17:16:03 +0200

    category card amir

--- client/src/Pages/StoreList.jsx
+++ client/src/Pages/StoreList.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { useEffect } from 'react';
-import { useState } from 'react';
 import ShoppingCard from '../components/ShoppingCard'
 import Stores from '../components/Stores';
 import SortOptions from '../components/SortOptions';
 import {
   useGetProductsQuery,
+  useCreateUserMutation,
+  useGetStoreQuery
+
 } from '../store/apiSlice'
-import { useEffect } from 'react';
 const StoreList = () => {
     const { data, isFetching,isLoading , isSuccess, isError } = useGetStoreQuery()
     const [createUser,response] =useCreateUserMutation()
@@ -28,11 +28,12 @@ const StoreList = () => {
     <div>
         <div className='border border-4 text-center mb-4  py-20 shadow-2xl'>
         <h1 className=' text-6xl mt-16'>קטגרויות</h1>
+        <button onClick={()=>createUser(newUser)}>create user</button>
     </div>
         <p className='text-2xl mb-12 text-center '>קטגרויות שונות</p>
 
      <div className='flex justify-center gap-3 mx-6 mb-10 sm:mx-2 md:w-["80%"]'>
-     { windowSize.innerWidth > 710 && <ShoppingCard/> }
+     <ShoppingCard/>
      <div className='flex flex-col grow w-8/12'>
         <div className='h-[30%]'><SortOptions/></div>
         <div className='h-full'>
@@ -47,8 +48,5 @@ const StoreList = () => {
 
   )
 }
-function getWindowSize() {
-    const {innerWidth, innerHeight} = window;
-    return {innerWidth, innerHeight};
-  }
+
 export default StoreList
--- client/src/components/Stores.jsx
+++ client/src/components/Stores.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {  useGetStoreQuery } from '../store/apiSlice'
 const Stores = () => {
   return ( 
     <>
