7e9013d5e947c9ef2d3cb005f8559458e9d20635
Meir Amara <meir757@gmail.com>
2022-12-22 14:24:57 +0200

    productCard

---
+++ client/src/components/ProductCard.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+
+
+function ProductCard(props) {
+  const [productToCart,setProductToCart]=useState()
+  const [qty, setQty] = useState(0)
+
+
+  return (
+    <div class="flex justify-center">
+  <div class="rounded-lg shadow-lg  border-red  border bg-white max-w-sm ">
+    <a href="#!">
+      <img class="rounded-t-lg" src={props.img} alt=""/>
+    </a>
+    <div class="p-6">
+      <h5 class="text-gray-900 text-xl font-medium mb-2 text-right">{props.name}</h5>
+      <p class="text-gray-700 text-base mb-4 text-right">
+      {props.description}
+      </p>
+      {props.quantity==0?<p className="text-right">אזל במלאי</p>:
+        <div>
+          <button className="text-right" onClick={()=>{if(qty<10&&qty<props.quantity )setQty(qty+1)}}>+</button><span>{qty}</span><button onClick={()=>{if(qty>0)setQty(qty-1)}}>-</button>
+        </div>}
+      <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default ProductCard
