ba13719aa530444fc7c80a11e6659a48fc2a14d0
Meir Amara <meir757@gmail.com>
2022-12-22 14:48:50 +0200

    nav bar

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -19,9 +19,9 @@ function ProductCard(props) {
       </p>
       {props.quantity==0?<p className="text-right">אזל במלאי</p>:
         <div>
-          <button className="text-right" onClick={()=>{if(qty<10&&qty<props.quantity )setQty(qty+1)}}>+</button><span>{qty}</span><button onClick={()=>{if(qty>0)setQty(qty-1)}}>-</button>
+          <button className="text-right rounded text-lime-500 hover:text-white hover:bg-lime-500 " onClick={()=>{if(qty<10&&qty<props.quantity )setQty(qty+1)}}>+</button><span>{qty}</span><button onClick={()=>{if(qty>0)setQty(qty-1)}}>-</button>
         </div>}
-      <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+      <button className=" " type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
     </div>
   </div>
 </div>
