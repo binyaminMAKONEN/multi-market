0ba2fabefd29fd72cf7a5b35dd72eeaff6a26d34
Meir Amara <meir757@gmail.com>
2022-12-25 23:27:51 +0200

    input cloudinary

--- client/src/index.js
+++ client/src/index.js
@@ -8,15 +8,18 @@ import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { CloudinaryContext, Image, Transformation } from 'cloudinary-react';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
  
     <BrowserRouter>
+    <CloudinaryContext cloudName={process.env.REACT_APP_CLOUDINARY_URL}>
       <ToastContainer />
-      <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID}>
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
         <Provider store={store}> <App /></Provider>
       </GoogleOAuthProvider>
+      </CloudinaryContext>
     </BrowserRouter>
   
 );
