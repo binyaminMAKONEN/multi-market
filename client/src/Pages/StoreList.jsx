a4d8019a03a17ad24631e8cb7445f7209f5e5b81
benjamin <bnymynmqwnn@gmail.com>
2022-12-25 16:27:59 +0200

    filterSerach + storeDesain

--- client/src/Pages/StoreList.jsx
+++ client/src/Pages/StoreList.jsx
@@ -5,14 +5,13 @@ import SortOptions from '../components/SortOptions';
 import {
   useGetStoreQuery
 } from '../store/apiSlice'
-
 const StoreList = () => {
     const { data ,isFetching,isSuccess,isError } = useGetStoreQuery()
     const myStorage = window.localStorage;
     const storeData = data?.filter(val=>val.storeType === myStorage.getItem('category'))
 
     let list;
-
+   let listMobile;
     if(isFetching){
       list = <div>Fetching</div>;
   }else if(isSuccess){
@@ -33,10 +32,9 @@ const StoreList = () => {
      <div className='flex justify-center  gap-3 mx-12 mb-10 sm:mx-2 md:mx-28'>
      <ShoppingCard/>
      <div className='flex flex-col grow w-8/12'>
-        <div className='h-[30%]'><SortOptions/></div>
+        <div className=' mb-4 '><SortOptions/></div>
         <div className=''>
         {list}
-
             </div>
      </div>
      </div>
