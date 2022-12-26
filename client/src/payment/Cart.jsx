15ebc9a5dcf00cb9ef4bd8b21f4fd5a1637a77cf
Aviv Adga <avivadga64@gmail.com>
2022-12-26 17:06:17 +0200

    aviv update

--- client/src/payment/Cart.jsx
+++ client/src/payment/Cart.jsx
@@ -15,9 +15,9 @@ export default function Cart() {
         </div>
         <div class="flex mt-10 mb-5 container  ">
           <h3 class="font-semibold text-gray-600 text-xs  uppercase w-2/4 container">Product Details</h3>
-          <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/4 text-center">Quantity</h3>
-          <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/4 text-center">Price</h3>
-          <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/4 text-center">Total</h3>
+          <h3 class="font-semibold  text-gray-600 text-xs uppercase w-1/4 text-center">Quantity</h3>
+          <h3 class="font-semibold  text-gray-600 text-xs uppercase w-1/4 text-center">Price</h3>
+          <h3 class="font-semibold  text-gray-600 text-xs uppercase w-1/4 text-center">Total</h3>
         </div>
         
 
